2999cb841ffa5eeec7c0b84923c135ba534c888e
mhngu23 <68138084+mhngu23@users.noreply.github.com>
2021-10-22 11:27:54 +1100

    Delete module-info.java

--- module-info.java
+++
@@ -1,9 +0,0 @@
-module Assignment2_s3716211 {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires java.sql;
-    requires junit;
-
-    opens GUI;
-}
