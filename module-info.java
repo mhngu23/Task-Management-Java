c5837654f5a5bf93e8c0b9bf75ffc8d3d5febb96
mhngu23 <68138084+mhngu23@users.noreply.github.com>
2021-10-22 11:26:22 +1100

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,9 @@
+module Assignment2_s3716211 {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    requires junit;
+
+    opens GUI;
+}
